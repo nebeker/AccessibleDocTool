30c9a86cafa5a078c751c3b47f5f82a7cc99e206
Nuno Nebeker <nunonebeker@gmail.com>
2021-05-18 23:30:50 +0100

    Build epub even if there's no cover, in bash.

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@ name=$(grep -m 1 "title: " title.txt | cut -c8-)
 name=${name//" "/"_"}
 
 pandoc title.txt text.md --toc -o out/$name.html
-pandoc title.txt text.md --css=accessible.css --epub-cover-image=cover.jpg --toc -o out/$name.epub
+if [[ -f cover.jpg ]]
+then
+    pandoc title.txt text.md --css=accessible.css --epub-cover-image=cover.jpg --toc -o out/$name.epub
+else
+    pandoc title.txt text.md --css=accessible.css --toc -o out/$name.epub
+fi
 pandoc title.txt text.md --toc --pdf-engine=xelatex --variable mainfont=Arial --variable fontsize=14pt -o out/$name.pdf
 pandoc title.txt text.md --toc --reference-doc=accessible.docx -o out/$name.docx
